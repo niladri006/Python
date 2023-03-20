c0d55a4d9c8f2e7e573e376be8c671a1aa15075f
niladri006 <128422336+niladri006@users.noreply.github.com>
2023-03-21 02:33:01 +0530

    Add files via upload

---
+++ Python [HackerRank]/Validating_Credit_Card_Numbers.py
@@ -0,0 +1,17 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+import re
+
+# compile the patterns
+pattern = re.compile(
+    r'^'
+    r'(?!.*(\d)(-?\1){3})'
+    r'[456]\d{3}'
+    r'(?:-?\d{4}){3}'
+    r'$')
+
+# using for loop to the input from user 
+for _ in range(int(input().strip())):
+    
+    #using pattern to search if the number is valid
+    print('Valid' if pattern.search(input().strip()) else 'Invalid')
